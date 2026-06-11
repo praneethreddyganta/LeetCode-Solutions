b539ff3b2ae339e5f000b4274e7f25dad217ea32
GANTA PRANEETH REDDY <praneethreddyganta@gmail.com>
2026-06-11 11:13:17 +0530

    Time: 497 ms (87.95%), Space: 22.2 MB (54.31%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -4,14 +4,16 @@ class Solution:
         n=len(nums)
         matrix=[]
         for i in range(n-2):
+            if nums[i]>0:
+                break
             if i>0 and nums[i]==nums[i-1]:
                 continue
             left=i+1
             right=n-1
-            target=-nums[i]
+            
             while left<right:
-                sum=nums[left]+nums[right]
-                if sum==target:
+                current_sum=nums[i]+nums[left]+nums[right]
+                if current_sum==0:
                     matrix.append([nums[i],nums[left],nums[right]])
                     left+=1
                     right-=1
@@ -19,7 +21,7 @@ class Solution:
                         left+=1
                     while left<right and nums[right]==nums[right+1]:
                         right-=1
-                elif sum < target:
+                elif current_sum < 0:
                     left+=1
                 else: 
                     right-=1
