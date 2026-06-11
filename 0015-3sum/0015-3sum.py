2f07cc642f5ebf067cf9b2f2c95416e04f1daace
GANTA PRANEETH REDDY <praneethreddyganta@gmail.com>
2026-06-11 11:00:04 +0530

    Time: 527 ms (83.37%), Space: 22.1 MB (93.81%) - LeetHub

---
+++ 0015-3sum/0015-3sum.py
@@ -0,0 +1,30 @@
+class Solution:
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
+        nums.sort()
+        n=len(nums)
+        matrix=[]
+        for i in range(n-2):
+            if i>0 and nums[i]==nums[i-1]:
+                continue
+            left=i+1
+            right=n-1
+            target=-nums[i]
+            while left<right:
+                sum=nums[left]+nums[right]
+                if sum==target:
+                    matrix.append([nums[i],nums[left],nums[right]])
+                    left+=1
+                    right-=1
+                    while left<right and nums[left]==nums[left-1]:
+                        left+=1
+                    while left<right and nums[right]==nums[right+1]:
+                        right-=1
+                elif sum < target:
+                    left+=1
+                else: 
+                    right-=1
+                
+        return matrix
+
+        
+        
