2b529f817961d0104f2de42b1936601dc9125b92
GANTA PRANEETH REDDY <praneethreddyganta@gmail.com>
2026-06-12 11:35:35 +0530

    Time: 1 ms (13.29%), Space: 19.2 MB (89.92%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -3,21 +3,19 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+        # BY USING DUTCH NATIONAL FLAG ALGORTITHM
         n=len(nums)
-        count0=0
-        count1=0
-        count2=0
-        for i in range(n):
-            if nums[i]==0:
-                count0+=1
-            elif nums[i]==1:
-                count1+=1
+        low=0
+        mid=0
+        high=n-1
+        while mid<=high:
+            if nums[mid]==0:
+                nums[mid],nums[low]=nums[low],nums[mid]
+                low+=1
+                mid+=1
+            elif nums[mid]==1:
+                mid+=1
             else:
-                count2+=1
-        for i in range(count0):
-            nums[i]=0
-        for i in range(count0,count0+count1):
-            nums[i]=1
-        for i in range(count0+count1,count0+count1+count2):
-            nums[i]=2
+                nums[mid],nums[high]=nums[high],nums[mid]
+                high-=1
         return nums
