bf0e59e6f88fd0f87ba850a3076da79471d87d51
GANTA PRANEETH REDDY <praneethreddyganta@gmail.com>
2026-07-15 00:21:47 +0530

    Time: 3 ms (90.08%), Space: 20.3 MB (45.85%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.py
+++ 0724-find-pivot-index/0724-find-pivot-index.py
@@ -1,6 +1,6 @@
 class Solution:
     def pivotIndex(self, nums: List[int]) -> int:
-        n=len(nums)
+        '''n=len(nums)
         left_arr=[0]
         prefix_sum=0
         for i in range(1,n):
@@ -15,8 +15,17 @@ class Solution:
             if left_arr[i]==right_arr[i]:
                 return i
         return -1
-
-
+'''
+        #GPT Given Time:O(N),Space:O(1)
+        n=len(nums)
+        total_sum=sum(nums)
+        left_sum=0
+        for i in range(n):
+            right_sum=total_sum-left_sum-nums[i]
+            if left_sum==right_sum:
+                return i
+            left_sum+=nums[i]
+        return -1
 
 
         
