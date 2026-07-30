d71d4136cd7bbf865dc90928897cf4168c1fd038
GANTA PRANEETH REDDY <praneethreddyganta@gmail.com>
2026-07-30 22:04:48 +0530

    Time: 0 ms (100%), Space: 19.2 MB (48.74%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.py
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.py
@@ -1,22 +1,29 @@
 class Solution:
     def minimumPushes(self, word: str) -> int:
-        k=8
-        c=0
+        '''
+            MY CODE
+            k=8
+            c=0
+            for i in range(len(word)):
+                if k>0:
+                    c+=1
+            
+                elif k>-8 :
+                    c+=2
+            
+                elif k>-16:
+                    c+=3
+            
+                else:
+                    c+=4
+                k-=1
+            return c
+        '''
+        #GPT GIVEN
+        count=0
         for i in range(len(word)):
-            if k>0:
-                c+=1
-            
-            elif k>-8 :
-                c+=2
-            
-            elif k>-16:
-                c+=3
-            
-            else:
-                c+=4
-            k-=1
-        return c
-            
+            count+=i//8+1
+        return count
 
 
         
