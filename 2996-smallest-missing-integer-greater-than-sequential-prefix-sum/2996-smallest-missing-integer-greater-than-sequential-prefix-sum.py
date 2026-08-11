1a96a593f8869800072ed6f3433696102e1bd84f
GANTA PRANEETH REDDY <praneethreddyganta@gmail.com>
2026-08-11 21:35:11 +0530

    Time: 0 ms (100%), Space: 19.3 MB (51.82%) - LeetHub

--- 2996-smallest-missing-integer-greater-than-sequential-prefix-sum/2996-smallest-missing-integer-greater-than-sequential-prefix-sum.py
+++ 2996-smallest-missing-integer-greater-than-sequential-prefix-sum/2996-smallest-missing-integer-greater-than-sequential-prefix-sum.py
@@ -1,15 +1,27 @@
 from collections import Counter
 class Solution:
     def missingInteger(self, nums: List[int]) -> int:
-        s=nums[0]
+        #My Code :Time: O(n),Space:O(n)
+        # s=nums[0]
+        # for i in range(1,len(nums)):
+        #     if nums[i]==nums[i-1]+1:
+        #         s+=nums[i]
+        #     else:
+        #         break
+        # freq=Counter(nums)
+        # while True:
+        #     if freq[s]==0:
+        #         return s
+        #     else:
+        #         s+=1
+        # A bit improved code but with same time & Space Complexity
+        sum=nums[0]
         for i in range(1,len(nums)):
             if nums[i]==nums[i-1]+1:
-                s+=nums[i]
+                sum+=nums[i]
             else:
                 break
-        freq=Counter(nums)
-        while True:
-            if freq[s]==0:
-                return s
-            else:
-                s+=1
+        s=set(nums)
+        while sum in s:
+            sum+=1
+        return sum
