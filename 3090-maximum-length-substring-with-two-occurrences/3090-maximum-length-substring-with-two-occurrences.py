3c325d0c333534d41e126b6c3f25a99c0f04f1ad
GANTA PRANEETH REDDY <praneethreddyganta@gmail.com>
2026-08-14 15:16:27 +0530

    Time: 4 ms (46.3%), Space: 19.3 MB (21.08%) - LeetHub

--- 3090-maximum-length-substring-with-two-occurrences/3090-maximum-length-substring-with-two-occurrences.py
+++ 3090-maximum-length-substring-with-two-occurrences/3090-maximum-length-substring-with-two-occurrences.py
@@ -1,19 +1,22 @@
 class Solution:
     def maximumLengthSubstring(self, s: str) -> int:
         n=len(s)
+        #GPT Helped with window_length 
+        freq={}
+        l=0
         max_count=0
-        for i in range(n):
-            count=0
-            freq={}
-            for j in range(i,n):
-                if s[j] not in freq:
-                    freq[s[j]]=1
-                    count+=1
-                else:
-                    freq[s[j]]+=1
-                    if freq[s[j]]<=2:
-                        count+=1
-                    else:
-                        break               
-            max_count=max(max_count,count)
+        for r in range(n):
+            if s[r] not in freq:
+                freq[s[r]]=1
+                
+            else:
+                freq[s[r]]+=1
+                
+            
+            while freq[s[r]]>2:
+                freq[s[l]]-=1
+                l+=1
+            window_length=r-l+1
+            max_count=max(max_count,window_length)
+            
         return max_count
