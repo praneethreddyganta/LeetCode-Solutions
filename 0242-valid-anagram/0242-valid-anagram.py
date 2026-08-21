0fde10c508b67703ff8b38d2a746ceb6952dec89
GANTA PRANEETH REDDY <praneethreddyganta@gmail.com>
2026-08-21 23:23:30 +0530

    Time: 13 ms (51.89%), Space: 19.4 MB (76.04%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,10 +1,24 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        freq1={}
+        '''freq1={}
         for val in s:
             freq1[val]=freq1.get(val,0)+1
         freq2={}
         for val in t:
             freq2[val]=freq2.get(val,0)+1
         return True if freq1==freq2 else False
+        '''
+        #GPT Given
+        if len(s)!=len(t):
+            return False
+        freq={}
+        for val in s:
+            freq[val]=freq.get(val,0)+1
+        for val in t:
+            freq[val]=freq.get(val,0)-1
+        for val in freq.values():
+            if val!=0:
+                return False
+        return True
+
         
