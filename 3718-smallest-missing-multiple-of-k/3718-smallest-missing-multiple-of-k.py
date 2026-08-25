95973d560ff09a5163e09abda116194f1d485b49
GANTA PRANEETH REDDY <praneethreddyganta@gmail.com>
2026-08-25 23:59:40 +0530

    Time: 0 ms (100%), Space: 19.3 MB (53.35%) - LeetHub

--- 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.py
+++ 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.py
@@ -1,13 +1,19 @@
 from collections import Counter
 class Solution:
     def missingMultiple(self, nums: List[int], k: int) -> int:
-        freq=Counter(nums)
+        # freq=Counter(nums)
+        # temp=k
+        # while True:
+        #     if temp not in freq:
+        #         return temp
+        #     else:
+        #         temp+=k
+        # Idea from Karthik Freq pedthey naku number and dhani frequency osthadhi but nak frequency  avasaram ledhu,just a value  kavali anthey
+        s=set(nums)
         temp=k
         while True:
-            if temp not in freq:
+            if temp not in s:
                 return temp
-            else:
+            else: 
                 temp+=k
-
-
         
