278955661993e1d53ab33d4efe9501d20c3d5c6c
GANTA PRANEETH REDDY <praneethreddyganta@gmail.com>
2026-09-05 14:12:23 +0530

    Time: 151 ms (76.82%), Space: 33.2 MB (32.73%) - LeetHub

--- 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.py
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.py
@@ -1,29 +1,22 @@
 class Solution:
     def firstStableIndex(self, nums: list[int], k: int) -> int:
-        min=[0]*len(nums)
-        
-        min[-1]=nums[-1]
-        
-        for j in range(len(nums)-2,-1,-1):
-            if nums[j]<min[j+1]:
-                min[j]=nums[j]
-            else:
-                min[j]=min[j+1]
+        n = len(nums)
+        # Idhi gpt ichindhi na badhamakam valla type cheyaley nenu 3 loops thos run chesina but 2 loops inka optimal basic ga  e idea 0ms latency speeed tho execute chesina valla code lo chusina
+        suffix_min = [0] * n
+        suffix_min[-1] = nums[-1]
 
-        max=[0]*len(nums)
-        max[0]=nums[0]
+        for i in range(n - 2, -1, -1):
+            suffix_min[i] = min(nums[i], suffix_min[i + 1])
 
-        # Check index 0
-        if max[0]-min[0] <= k:
+        prefix_max = nums[0]
+
+        if prefix_max - suffix_min[0] <= k:
             return 0
-        
-        for i in range(1,len(nums)):
-            if nums[i]>max[i-1]:
-                max[i]=nums[i]
-            else:
-                max[i]=max[i-1]
 
-            if max[i]-min[i]<=k:
+        for i in range(1, n):
+            prefix_max = max(prefix_max, nums[i])
+
+            if prefix_max - suffix_min[i] <= k:
                 return i
-                
+
         return -1
