bcd8de718177a42f8802e8e0325f7fbd48b10f71
GANTA PRANEETH REDDY <praneethreddyganta@gmail.com>
2026-09-07 22:33:07 +0530

    Time: 7 ms (80.86%), Space: 19.5 MB (77.82%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,7 +1,9 @@
 class Solution:
     import re
     def isPalindrome(self, s: str) -> bool:
-        s=s.lower()
+        # My Code 
+        #Time and space:O(N)
+        ''' s=s.lower()
         s1=re.sub(r'[^a-zA-Z0-9]','',s)
         s2=list(s1)
         temp=s2.copy()
@@ -11,4 +13,18 @@ class Solution:
             s2[left],s2[right]=s2[right],s2[left]
             left+=1
             right-=1
-        return True if s2==temp else  False
+        return True if s2==temp else  False'''
+        #Gemini Optimsed 
+        #Time:O(n) and space O(1)
+        left=0
+        right=len(s)-1
+        while left<right:
+            while left<right and  not s[left].isalnum():
+                left+=1
+            while left<right and not s[right].isalnum():
+                right-=1
+            if s[left].lower()!=s[right].lower():
+                return False
+            left+=1
+            right-=1
+        return True
