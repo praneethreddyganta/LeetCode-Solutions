dbd2f120546487ee4cbdac74815e1a26391aaf73
GANTA PRANEETH REDDY <praneethreddyganta@gmail.com>
2026-09-14 19:11:52 +0530

    Time: 13 ms (23.05%), Space: 21.6 MB (18.98%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,7 +1,21 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         #GPT Helped
+        '''
         mp={}
         for i in nums:
             mp[i]=mp.get(i,0)+1
-        return max(mp,key=mp.get)       
+        return max(mp,key=mp.get)
+        '''
+        #boyer-moore-majority-vote-algorithm
+        candidate=None
+        count=0
+        #idhi kuda gpt ey ichindhi 
+        for i in nums:
+            if count==0:
+                candidate=i
+            if candidate==i:
+                count+=1
+            else:
+                count-=1
+        return candidate
