e9ddfcb575a0733ab1ef535b2a5dbd1218375f67
GANTA PRANEETH REDDY <praneethreddyganta@gmail.com>
2026-09-15 01:01:03 +0530

    Time: 6 ms (98.77%), Space: 22.2 MB (53.34%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,12 @@
+from collections import Counter
+class Solution:
+    #Motham youtube
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        group={}
+        for word in strs:
+            sorted_word=''.join(sorted(word))
+            if sorted_word not in group:
+                group[sorted_word]=[]
+            group[sorted_word].append(word)
+        return list(group.values())
+        
